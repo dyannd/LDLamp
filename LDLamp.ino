1dcec6dc60d239cff531420ca0f743c57355837e
Anh Nguyen <anhdng8@gmail.com>
2023-09-07 21:23:09 -0400

    refactor color files

--- LDLamp.ino
+++ LDLamp.ino
@@ -1,6 +1,7 @@
 //import the libraries
 #include <Adafruit_NeoPixel.h>
 #include "config.h"
+#include "color.h"
 #include <NeoPixelBrightnessBus.h>
 
 //define the Neopixel pin, this will be different then the pin we soldered the Neopixels to, as they are marked differently
@@ -8,6 +9,10 @@
 #define INITIALBRIGHTNESS 64
 #define GPIOPIN 12  //pin on the wesmos chip, 12 is D6
 #define LEDS 12
+
+//set one of the lamps to 1, the other to 2 (change when uploading sketch to different chips)
+int lampVal = 2;
+
 //initialize the information for the Neopixels and Adafruit IO
 NeoPixelBrightnessBus< NeoGrbFeature, NeoEsp8266Dma800KbpsMethod > lightStrip(LEDS, PIN);
 
@@ -20,41 +25,9 @@ int i_breath;
 
 char msg[50];  //  Custom messages for Adafruit IO
 
-const int max_intensity = 255;  //  Max intensity
-//  Color definitions
-RgbColor red(max_intensity, 0, 0);
-RgbColor green(0, max_intensity, 0);
-RgbColor blue(0, 0, max_intensity);
-RgbColor purple(200, 0, max_intensity);
-RgbColor cyan(0, max_intensity, max_intensity);
-RgbColor yellow(max_intensity, 200, 0);
-RgbColor white(max_intensity, max_intensity, max_intensity);
-RgbColor pink(255, 20, 30);
-RgbColor orange(max_intensity, 50, 0);
-RgbColor black(0, 0, 0);
-
-RgbColor colors[] = {
-  red,
-  orange,
-  yellow,
-  green,
-  cyan,
-  blue,
-  purple,
-  pink,
-  white,
-  black
-};
-
-//how long we want the lamps to stay on when activated in millis
-const unsigned long interval = 60000;
-
 //setup the timers and status for the lamp, in millis
 unsigned long initialTime = 0;
 int tap = 0;
-
-//set one of the lamps to 1, the other to 2 (change when uploading sketch to different chips)
-int lampVal = 2;
 
 //the value that should activate the lamp (is 2 for the first lamp, and 1 for the second)
 int recVal{ 0 };
@@ -77,7 +50,7 @@ const int on_time = 900000;
 // Disconection timeout
 unsigned long currentMillis;
 unsigned long previousMillis = 0;
-const unsigned long conection_time_out = 300000;  // 5 minutos
+const unsigned long conection_time_out = 300000;  // 5 minutes
 
 void setup() {
 
@@ -98,15 +71,14 @@ void setup() {
   lightStrip.Begin();
   lightStrip.Show();  // Initialize all pixels
 
-  //setup the touch sensor as a interrupt and input
+  //setup the touch sensor as an input
   pinMode(GPIOPIN, INPUT);
-  // attachInterrupt(digitalPinToInterrupt(GPIOPIN), touch, CHANGE);  //executes the touch function when event changes on GPIOpin
 
   //start connecting to Adafruit IO
   Serial.print("Connecting to Adafruit IO");
   io.connect();
 
-  //get the status of the value in Adafruit IO
+  //get the status of the value in Adafruit IO when a message is received
   lamp->onMessage(handleMessage);
 
   //connect to Adafruit IO and play the "spin" animation to show it's connecting until and connection is established
@@ -117,7 +89,7 @@ void setup() {
   }
   off();
 
-  //when a connection to Adafruit IO is made write the status in the Serial monitor and flash the Neopixels white
+  //when a connection to Adafruit IO is made write the status in the Serial monitor and flash the Neopixels
   Serial.println();
   Serial.println(io.statusText());
   // Animation
@@ -149,8 +121,7 @@ void loop() {
       // Wait
     case 0:
       currentState = digitalRead(GPIOPIN);
-      if (lastState == LOW && currentState == HIGH)  // Button is pressed
-      {
+      if (lastState == LOW && currentState == HIGH) {  // Button is pressed{
         pressedTime = millis();
       } else if (currentState == HIGH) {
         releasedTime = millis();
@@ -293,73 +264,35 @@ void loop() {
       state = 0;
       break;
   }
-
-  //tap = 1 means that we have established that the other lamp was tapped
-  // if (tap == 1) {
-
-  //   //check to see if the timer if over our predefined interval. If it is, turn off the Neopixels and reset the tap status
-  //   if (currentTime - initialTime >= interval) {
-  //     off();
-  //     tap = 0;
-  //   } else {
-  //     //if the timer isn't over the predefined interval, continue playing the rainbow animation at a slow speed
-  //     rainbow(200);
-  //   }
-  // }
 }
-
-
-//the interrupt program that runs when the touch sensor is activated
-// IRAM_ATTR void touch() {
-
-//   //while the touch sensor is activated, save the lampVal (either 1 or 2) to the Adafruit IO feed and turn the Neopixels to purple
-//   while (digitalRead(GPIOPIN) == 1) {
-
-//     lamp->save(lampVal);
-//     Serial.print("This is lamp number ");
-//     Serial.print(lampVal);
-//     Serial.println();
-//     for (int i = 0; i < LEDS; i++) {
-//       lightStrip.SetPixelColor(i, 102, 0, 204);
-//     }
-
-//     lightStrip.Show();
-
-//     //once the touch sensor isn't activated, send a 0 back to the Adafruit IO feed.
-//   }
-//   if (digitalRead(GPIOPIN) == 0) {
-
-//     lamp->save(0);
-
-//     for (int i = 0; i < LEDS; i++) {
-
-//       lightStrip.SetPixelColor(i, 0, 0, 0);
-//     }
-
-//     lightStrip.Show();
-//   }
-// }
-
 
 //code that tells the ESP8266 what to do when it recieves new data from the Adafruit IO feed
 void handleMessage(AdafruitIO_Data *data) {
-
-  Serial.print("received <- ");
-
   //convert the recieved data to an INT
   int reading = data->toInt();
-
-  //if the received value is equal to the recVal, and the lamp status is currently off, change the status to on and renew the timer
-  if (reading == recVal && tap == 0) {
-
-    Serial.println("TAP");
-    initialTime = millis();
-    Serial.println(initialTime);
-    tap = 1;
-
-    //if we recieve a value but the lamp is already on, nothing happens
-  } else {
-    Serial.println("LOW");
+  if (reading == 66) {
+    sprintf(msg, "L%d: rebooting", lampVal);
+    lamp->save(msg);
+    lamp->save(0);
+    ESP.restart();
+  } else if (reading == 100) {
+    sprintf(msg, "L%d: ping", lampVal);
+    lamp->save(msg);
+    lamp->save(0);
+  } else if (reading == 420 + recVal) {
+    sprintf(msg, "L%d: pulse received", lampVal);
+    lamp->save(msg);
+    lamp->save(0);
+    pulse(selected_color);
+  } else if (reading != 0 && reading / 10 != lampVal) {
+    // Is it a color msg?
+    if (state == 0 && reading != 1) {
+      state = 9;
+      selected_color = reading - recVal;
+    }
+    // Is it an answer?
+    if (state == 5 && reading == 1)
+      state = 6;
   }
 }
 // 50% intensity
@@ -426,36 +359,6 @@ void off() {
   }
   lightStrip.Show();
 }
-
-
-// //The code that creates the gradual color change animation in the Neopixels (thank you to Adafruit for this!!)
-// void rainbow(uint8_t wait) {
-//   uint16_t i, j;
-
-//   for (j = 0; j < 256; j++) {
-//     for (i = 0; i < LEDS; i++) {
-//       lightStrip.SetPixelColor(i, Wheel((i + j) & 255));
-//     }
-//     lightStrip.Show();
-//     delay(wait);
-//   }
-// }
-
-
-// //complicated geometry or something to figure out the color values
-// uint32_t Wheel(byte WheelPos) {
-//   WheelPos = 255 - WheelPos;
-//   if (WheelPos < 85) {
-//     return lightStrip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-//   }
-//   if (WheelPos < 170) {
-//     WheelPos -= 85;
-//     return lightStrip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-//   }
-//   WheelPos -= 170;
-//   return lightStrip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-// }
-
 
 //code to flash the Neopixels when a stable connection to Adafruit IO is made
 void flash(int ind) {
